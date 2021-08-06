170b81242c9f4900dd4ffb1db6bde2138b5d47a0
Marc Scott <marc@raspberrypi.org>
2021-08-06 10:21:50 +0100

    Update install_script.sh

--- en/resources/install_script.sh
+++ en/resources/install_script.sh
@@ -1,5 +1,4 @@
 echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install libedgetpu1-std
-sudo apt-get install python3-pycoral
+sudo apt-get install libedgetpu1-std python3-pycoral -Y
